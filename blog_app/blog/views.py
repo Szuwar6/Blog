ba5a0dcc63869bff867b3d89faabbfdc326b2421
Marek <marekszubartowicz@gmail.com>
2023-03-01 14:14:21 +0100

    after black

--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -1,18 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
-
 from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
     UpdateView,
-    DeleteView,
-    TemplateView,
+    DeleteView
 )
 
 from .models import Article
 
 
+@login_required
 def about(request):
     return render(request, "blog/about.html", {"title": "About"})
 
